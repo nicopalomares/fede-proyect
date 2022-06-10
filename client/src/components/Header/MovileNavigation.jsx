2a93edcea427ad36c2b841f61bdd08d72aa4de3a
unknown <you@example.com>
2022-06-10 09:18:49 -0300

    terminado

--- client/src/components/Header/MovileNavigation.jsx
+++ client/src/components/Header/MovileNavigation.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import NavLinks from "./NavLinks";
 import style from "./NavBar.module.css";
 import {GiHamburgerMenu} from "react-icons/gi"
-import {IoMdCloseCircle} from "react-icons/io"
+import {IoMdClose} from "react-icons/io"
 
 export default function MovileNavigation() {
   const [open,setOpen] = useState(false)
-  let iconClose = <IoMdCloseCircle color="#FCF267" className={style.close} onClick={()=>setOpen(!open)}/>
+  let iconClose = <IoMdClose color="#FCF267" className={style.close} cursor="pointer" onClick={()=>setOpen(!open)}/>
   let iconHamburger = <GiHamburgerMenu className={style.hamburger} onClick={()=>setOpen(!open)}/>
   const closeMobileMenu = () => setOpen(false)
   return (
